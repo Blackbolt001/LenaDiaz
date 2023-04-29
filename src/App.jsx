701ad8bcae3647efc55beee92b247676c90ce09c
Joseph Gossett <105749663+Blackbolt001@users.noreply.github.com>
2023-04-29 19:29:15 -0400

    adding video background to header/announcment

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { Navigation } from "./components/navigation";
-import { Header } from "./components/header";
-import { Features } from "./components/features";
-import { About } from "./components/about";
-import { Services } from "./components/services";
-import { Gallery } from "./components/gallery";
-import { Reviews } from "./components/reviews";
-import { Team } from "./components/Team";
-import { Contact } from "./components/contact";
-import JsonData from "./data/data.json";
-import SmoothScroll from "smooth-scroll";
-import "./App.css";
+import Home from "./pages/Home";
 
-export const scroll = new SmoothScroll('a[href*="#"]', {
-  speed: 1000,
-  speedAsDuration: true,
-});
+
 
 const App = () => {
-  const [landingPageData, setLandingPageData] = useState({});
-  useEffect(() => {
-    setLandingPageData(JsonData);
-  }, []);
-
-  return (
-    <div>
-      <Navigation />
-      <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
-      <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
-      <Gallery data={landingPageData.Gallery} />
-      <Reviews data={landingPageData.Reviews} />
-      <Team data={landingPageData.Team} />
-      <Contact data={landingPageData.Contact} />
-    </div>
-  );
-};
+  return( 
+    <Home/>
+)};
 
 export default App;
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -134,11 +134,6 @@ export const Contact = (props) => {
                       <i className="fa fa-twitter"></i>
                     </a>
                   </li>
-                  <li>
-                    <a href={props.data ? props.data.youtube : "/"}>
-                      <i className="fa fa-youtube"></i>
-                    </a>
-                  </li>
                 </ul>
               </div>
             </div>
